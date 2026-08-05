24f2d92e5649110c3a3bcf8368bc485c639ad341
Shreyasi Sen <shreyasi174@gmail.com>
2026-08-06 01:25:11 +0530

    Implement 2D array traversal in C

---
+++ 2d_array_traversing.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[2][3] = {
+        {1, 2, 3},
+        {4, 5, 6}
+    };
+
+    int i, j;
+
+    printf("Elements of 2D Array:\n");
+
+    for(i = 0; i < 2; i++)
+    {
+        for(j = 0; j < 3; j++)
+        {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
