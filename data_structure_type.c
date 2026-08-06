ded691fb5ae34bcdc3ba0e772d555d61987cf649
Shreyasi Sen <shreyasi174@gmail.com>
2026-08-06 22:15:50 +0530

    Add data structure type identification using strings

--- data_structure_type.c
+++ data_structure_type.c
@@ -3,31 +3,31 @@
 
 int main()
 {
-    char ds[20];
+    char str[20];
 
     printf("Enter the data structure name: ");
-    scanf("%s", ds);
+    scanf("%s", str);
 
-    if (strcmp(ds, "Array") == 0)
+    if (strcmp(str, "Array") == 0)
         printf("Array is a Linear Data Structure");
 
-    else if (strcmp(ds, "Stack") == 0)
+    else if (strcmp(str, "Stack") == 0)
         printf("Stack is a Linear Data Structure");
 
-    else if (strcmp(ds, "Queue") == 0)
+    else if (strcmp(str, "Queue") == 0)
         printf("Queue is a Linear Data Structure");
 
-    else if (strcmp(ds, "LinkedList") == 0)
+    else if (strcmp(str, "LinkedList") == 0)
         printf("Linked List is a Linear Data Structure");
 
-    else if (strcmp(ds, "Tree") == 0)
+    else if (strcmp(str, "Tree") == 0)
         printf("Tree is a Non-Linear Data Structure");
 
-    else if (strcmp(ds, "Graph") == 0)
+    else if (strcmp(str, "Graph") == 0)
         printf("Graph is a Non-Linear Data Structure");
 
-    else if (strcmp(ds, "int") == 0 || strcmp(ds, "char") == 0 ||
-             strcmp(ds, "float") == 0 || strcmp(ds, "double") == 0)
+    else if (strcmp(str, "int") == 0 || strcmp(str, "char") == 0 ||
+             strcmp(str, "float") == 0 || strcmp(str, "double") == 0)
         printf("It is a Primitive Data Type");
 
     else
