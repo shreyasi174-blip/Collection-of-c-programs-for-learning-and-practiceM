fd888d659665e439e1ae36d892a56b58ff8a0456
Shreyasi Sen <shreyasi174@gmail.com>
2026-08-19 15:15:27 +0530

    Implement linear search using pointer and function in C

--- Linear_search_using_pointer.c
+++ Linear_search_using_pointer.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
-void traversearray(int *arr,int size){
-   for(int i=0;i<size;i++){
-    printf("Elements at index %d: %d\n",i,arr[i]);
-   }
+int linearsearch(int arr[],int size,int key){
+    for(int i=0;i<size;i++){
+        if(arr[i]==key){
+            return i;
+        }
+    }
+    return -1;
 }
+int main()
+{
+    int Arr[100]={1,2,5,7,8,9};
+    int size=sizeof(Arr) / sizeof(Arr[0]);
+    int key=5;
+    int result=linearsearch(Arr,size,key);
+    if(result!=-1){
+        printf("Element %d found at index: %d\n",key,result);
+    }else{
+         printf("Element %d not found at index.\n",key);
+    }
 
-int main(){
-    int Array[]={10,20,30,40,50};
-    int length=sizeof(Array) / sizeof(Array[0]);
-    printf("Traversing the array using function: \n");
-    traversearray(Array,length);
     return 0;
 }
